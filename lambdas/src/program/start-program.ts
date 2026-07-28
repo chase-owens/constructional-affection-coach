fc0b2c49ba9844b2dd59e6d59aacf8c29849b91c
Chase Owens <chasejonathanowens@gmail.com>
2026-07-28 17:55:17 -0500

    feat: remove unnecessary code

--- lambdas/src/program/start-program.ts
+++ lambdas/src/program/start-program.ts
@@ -207,8 +207,6 @@ export const handler = async (event: StartProgramEvent): Promise<void> => {
           message: issue.message,
         })),
       });
-
-      continue;
     }
 
     if (lastValidationError) {
